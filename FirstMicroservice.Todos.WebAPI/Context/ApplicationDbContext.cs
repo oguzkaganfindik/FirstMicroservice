997771645a14ebe298bd3d812dcf0140116b92c9
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-08-24 20:30:14 +0300

    Todo Projesi Ayaga Kaldirildi ve Postman ile Api Testinden Basariyla Gecti

---
+++ FirstMicroservice.Todos.WebAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstMicroservice.Todos.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMicroservice.Todos.WebAPI.Context
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
